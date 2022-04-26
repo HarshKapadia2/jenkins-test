30719d30c550596bbf4fdac920b0f3fcfed4a326
Harsh Kapadia <harshgkapadia@gmail.com>
2022-04-26 18:25:48 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+	public static void main (String args[]) {
+		int num_1 = Integer.parseInt(args[0]);
+		int num_2 = Integer.parseInt(args[1]);
+
+    int sum = num_1 + num_2;
+
+		System.out.println("Sum = " + sum);
+	}
+}
